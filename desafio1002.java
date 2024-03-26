f322552b3164fb818fe8d2a2685cc3f0d064fbb0
Daniel Paixão <143643747+dannpaixa@users.noreply.github.com>
2024-03-26 15:45:08 -0300

    Alteração

--- desafio1002.java
+++ desafio1002.java
@@ -3,8 +3,8 @@ import java.util.Locale;
 
 public class desafio1002 {
     public static void main(String[] args) {
-    /* Apresente a mensagem "A=" seguida do valor da variÃ¡vel, conforme exemplo abaixo, 
-    *com quatro casas apÃ³s a vÃ­rgula. Use todas as variÃ¡veis â€‹â€‹de precisÃ£o double.
+    /* Apresente a mensagem "A=" seguida do valor da variável, conforme exemplo abaixo, 
+    *com quatro casas após a ví­rgula. Use todas as variáveis com taxa de precisão double.
     */
     Locale.setDefault(Locale.US);
     Scanner entrada = new Scanner(System.in);
@@ -12,18 +12,18 @@ public class desafio1002 {
     double pi = 3.14159;
 
     System.out.print("");
-    R = entrada.nextDouble(); // Recebe o valor escrito pelo usuÃ¡rio no terminal
+    R = entrada.nextDouble(); // Recebe o valor escrito pelo usuário no terminal
     
     
-    //A =  pi*(R*R); //Classe e mÃ©todo responsÃ¡veis por calcular o quadrado de X variÃ¡vel
+    //A =  pi*(R*R); //Classe e método responsáveis por calcular o quadrado de X variável
     A = pi*Math.pow(R, 2);
     //DecimalFormat df = new DecimalFormat("A=");
     //df.setMaximumFractionDigits(4);
     //System.out.println(df.format(A));
     
     System.out.printf("A=%.4f\n", A);
-    /*O printf Ã© o mÃ©todo que permite formatar o tipo de String que serÃ¡ impressa
-     * Neste caso, depois do ponto decimal, serÃ¡ impressa 4 casas
+    /*O printf é o método que permite formatar o tipo de String que serão impressa
+     * Neste caso, depois do ponto decimal, serão impressas 4 casas
     */
     
     entrada.close();
